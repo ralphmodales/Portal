86100ee921065875c12f0edfa1c7450646c7231a
ralphmodales <ralphmodales@gmail.com>
2024-11-22 17:26:36 +0800

    Some changes

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     <main className="flex-1">
       <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-white overflow-hidden relative">
         <div
-          className="absolute inset-0 z-0 top-10 rounded-xl"
+          className="absolute inset-0 z-0 top-10 rounded-3xl"
           style={{
             backgroundImage: "url('/wave.jpg')",
             backgroundSize: 'cover',
@@ -36,7 +36,7 @@ export default function Home() {
             </div>
             <div className="flex space-x-4">
               <a href="/systems">
-                <Button className="bg-white text-black hover:bg-black hover:text-white">Get Started</Button>
+                <Button className="bg-white text-black hover:bg-black hover:text-white rounded-2xl">Get Started</Button>
               </a>
             </div>
           </div>
