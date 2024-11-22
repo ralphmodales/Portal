0d51281d0205214064e1e7a4a1bb3e13930ccc77
ralphmodales <ralphmodales@gmail.com>
2024-11-22 22:35:06 +0800

    Foundation for system page added

--- app/systems/page.tsx
+++ app/systems/page.tsx
@@ -1,12 +1,21 @@
 export default function Systems() {
   return (
     <main className="flex-1">
-      <section className="w-full py-20 bg-white overflow-hidden">
-        <div className="container mx-auto relative z-50">
-          <div className="flex flex-col items-start text-left px-4">
-            <h1 className="text-6xl font-bold tracking-tighter text-black">
-              Systems
-            </h1>
+      <section className="w-full bg-white py-20">
+        <div className="container mx-auto">
+          <div className="flex flex-col md:grid md:grid-cols-2 gap-4 md:gap-8 items-start">
+            <div>
+              <h1 className="text-5xl md:text-6xl font-bold tracking-tight text-black">
+                Systems
+              </h1>
+            </div>
+            <div className="md:flex md:items-center md:justify-end">
+              <h2 className="text-2xl md:text-2xl font-semibold tracking-tight text-black">
+                What is impossible for you
+                <br />
+                is not impossible for me.
+              </h2>
+            </div>
           </div>
         </div>
       </section>
